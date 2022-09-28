42bcbf0bfb1e209ed0d9c0f6ba643b24d569e6bc
Kushagra Chaturvedi <kushagrachaturvedi15@gmail.com>
2022-09-28 11:53:24 +0530

    Update

--- Regressor/LinearRegressor.py
+++ Regressor/LinearRegressor.py
@@ -1,63 +1,70 @@
-class Regressor:
+from multiprocessing import dummy
+import numpy  as np
+
+class LinearRegressor:
     def __init__(self, weight=0, bias=0):
         self.w = weight
         self.b = bias
     
-    def trainModel(self, X, Y, LR=0.01):
+    def stochasticGradient(self, X, Y, LR=0.0001):
+        # variable init
         n = len(X)
-        pdw = 0
-        pdb = 0
-        for i in range(n):
-            x = X.iloc[i]
-            y = Y.iloc[i]
-            pdb += -(2/n) * (y - (self.w * x) + self.b)
-            pdw += -(2/n) * (y - (self.w * x) + self.b) * x
-            # print(pdw, pdb)
+        X = np.array(X)
+        Y = np.array(Y)
+
+        # Claculating slope of loss function with respect to Weight and Biases.
+        pdw = np.sum(-(2/n) * (Y - (X * self.w + self.b)) * X)    
+        pdb = np.sum(-(2/n) * (Y - (X * self.w + self.b)))
+
+        # Updating weights and biases.
         self.w -= (LR * pdw) 
         self.b -= (LR * pdb)
-        return 0 
 
+    
+    def trainModel(self, X, Y, LR=0.0001, E=10):
+        for i in range(E):
+            self.stochasticGradient(X, Y, LR)
+
+    def modelError(self, X, Y):
+        n = len(X)
+        X = np.array(X)
+        Y = np.array(Y)        
+
+        error = ((np.sum(Y - self.predict(X)) / n) / (np.sum(X)/n)) * 100
+        return error
+    
     def predict(self, x):
         y = (self.w * x) + self.b
         return y
 
+class MVRegressor():
+    pass
 
-if __name__ == "__main__":
-    import numpy as np
+if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import pandas as pd
-
-    # Dummy Data
-    train = pd.read_csv("Dummy-data/train.csv")
-    train.dropna(inplace=True)
-    Y = train['y']
-    X = train['x']
     
-
-    # Normalize Data
-    # sy = sum(Y)
-    # Y = list(map(lambda x: x/sy, Y))
-
-    # X = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # Y = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+    dummy_train = pd.read_csv('Regressor/Dummy-data/train.csv').dropna()
+    dummy_test = pd.read_csv('Regressor/Dummy-data/test.csv').dropna()
     
-    # Main
-    reg = Regressor(weight=0, bias=0)
-    print(f"Weight: {reg.w} Bias: {reg.b}")
+    xTrain = dummy_train['x']
+    yTrain = dummy_train['y']
+    xTest = dummy_test['x']
+    yTest = dummy_test['y']
+  
+    reg = LinearRegressor(weight=0, bias=0)
 
-    epochs = 1000
-    for i in range(epochs):
-        reg.trainModel(X, Y, LR=.0001)
-        print(f"Weight: {reg.w} Bias: {reg.b}")
-    
-    slope = reg.w
-    c = reg.b
+    print(f"\nBefore Training\nModel error: {reg.modelError(xTest, yTest)}\n\n")
+    reg.trainModel(xTrain, yTrain, LR=0.0001)
+    print(f"After Traning\nModel error: {reg.modelError(xTest, yTest)}")
 
-    q = 40
-    print(f"prediction for X:{q} is {reg.predict(q)}")
 
-    # Ploting
-    plt.scatter(X, Y)
-    plt.plot(list(range(0, 100)), [reg.w * i + reg.b for i in range(0, 100)], color="red") 
-
+    plt.scatter(xTest, yTest)
+    x = np.array([0, 100])
+    plt.plot(x, reg.predict(x), color='red')
     plt.show()
+
+
+
+   
+
